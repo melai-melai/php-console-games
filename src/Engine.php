2eb84c266beb10968d9e48992c1d5adc63817cfd
melai-melai <melai1777@yandex.ru>
2020-01-17 18:20:19 +0300

    Add Happy Ticket Game

--- src/Engine.php
+++ src/Engine.php
@@ -5,10 +5,12 @@ namespace PhpConsoleGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
+use function PhpConsoleGames\Games\HappyTicketGame\isHappy;
+
 function run()
 {
 	line("Hello, my friend! \n");
-	$answer = prompt("Let's play? \n", false, "Your answer [yes/no]: ");
+	$answer = prompt("Let's play? \n", false, "Your answer [yes|no]: ");
 	if ($answer === "no") {
 		line("See you, my friend! \n");
 		exit;
@@ -21,11 +23,18 @@ function run()
 	line("I have two games for you, %s! \n", $name);
 	line("[1] - Happy ticket game \n");
 	line("[2] - Parity \n");
-	$game = prompt("What game do you want to play? \n", false, "Your answer [1/2]: ");
+	$game = prompt("What game do you want to play? \n", false, "Your answer [1|2]: ");
 
 	switch ($game) {
 		case "1":
 			line("Here will be 'Happy ticket game' \n");
+			$ticket = prompt("Enter your ticket number: \n", false, "Your answer: ");
+			$answer = isHappy($ticket);
+			if ($answer) {
+				line("Congratulations! Your ticket %s is happy!' \n", $ticket);
+			} else {
+				line("Your ticket %s is not happy! =(' \n", $ticket);
+			}
 			break;
 		case "2":
 			line("Here will be 'Parity Game' \n");
@@ -33,4 +42,13 @@ function run()
 		default:
 			line("No, I don't have the game with number %s. \n", $game);
 	}
+
+	$answer = prompt("Do you want to exit or repeat again? \n [1] - exit, [2] - repeat \n", false, "Your answer [1|2]: ");
+	if ($answer === '1') {
+		line("See you, my friend! \n");
+		exit;
+	} else {
+		run();
+	}
+
 }
--- src/Games/HappyTicketGame.php
+++ src/Games/HappyTicketGame.php
@@ -1,18 +1,18 @@
 <?php
 
-// namespace PhpConsoleGames;
+namespace PhpConsoleGames\Games\HappyTicketGame;
 
-// function isHappy(string $number)
-// {
-//     $length = mb_strlen($number);
-//     if ($length === 0 || $length % 2 !== 0) {
-//         return false;
-//     }
+function isHappy(string $number)
+{
+    $length = mb_strlen($number);
+    if ($length === 0 || $length % 2 !== 0) {
+        return false;
+    }
 
-//     $balance = 0;
-//     for ($i = 0, $j = $length - 1; $i < $j; $i += 1, $j -= 1) {
-//         $balance += $number[$i] - $number[$j];
-//     }
+    $balance = 0;
+    for ($i = 0, $j = $length - 1; $i < $j; $i += 1, $j -= 1) {
+        $balance += $number[$i] - $number[$j];
+    }
 
-//     return $balance === 0;
-// }
+    return $balance === 0;
+}
